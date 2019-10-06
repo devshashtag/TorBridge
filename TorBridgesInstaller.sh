b9dc7baf4c7d3609fedb8818c4642c35f8fee975
MicroRobotProgramer <programmert3ster@gmail.com>
2019-10-06 17:24:11 +0330

    remove command comments

--- TorBridgesInstaller.sh
+++ TorBridgesInstaller.sh
@@ -2,15 +2,15 @@
 
 # installer tor bridges
 
-# mkdir -p ~/.local/bin
-# cd ~/.local/bin
+mkdir -p ~/.local/bin
+cd ~/.local/bin
 
-# download files
-# curl -s -o get-tor-bridges https://raw.githubusercontent.com/MicroRobotProgrammer/TorBridge/master/TorBridges.sh
-# curl -s -o remove-broken-bridges https://raw.githubusercontent.com/MicroRobotProgrammer/TorBridge/master/RemoveBrokenBridges.sh
+download files
+curl -s -o get-tor-bridges https://raw.githubusercontent.com/MicroRobotProgrammer/TorBridge/master/TorBridges.sh
+curl -s -o remove-broken-bridges https://raw.githubusercontent.com/MicroRobotProgrammer/TorBridge/master/RemoveBrokenBridges.sh
 
-# chmod +x remove-broken-bridges
-# chmod +x get-tor-bridges
+chmod +x remove-broken-bridges
+chmod +x get-tor-bridges
 
 [[ -z $(echo $PATH|grep $HOME/.local/bin) ]] &&
 {
