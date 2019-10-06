ad2907f2d4cd7ffdca97009cf3aa89cc597f66d6
MicroRobotProgramer <programmert3ster@gmail.com>
2019-10-06 17:47:50 +0330

    installation problems fixed

--- TorBridgesInstaller.sh
+++ TorBridgesInstaller.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
 # installer tor bridges
-
 mkdir -p ~/.local/bin
 cd ~/.local/bin
 
-download files
+# download files
 curl -s -o get-tor-bridges https://raw.githubusercontent.com/MicroRobotProgrammer/TorBridge/master/TorBridges.sh
 curl -s -o remove-broken-bridges https://raw.githubusercontent.com/MicroRobotProgrammer/TorBridge/master/RemoveBrokenBridges.sh
-
+# check files
+if [[ ! -e get-tor-bridges && ! -e remove-broken-bridges ]]; then
+    echo -e "\e[1;31mfiles are not available . Please Check the Permission or Connection.\e[m"
+    exit 1
+fi
 chmod +x remove-broken-bridges
 chmod +x get-tor-bridges
 
-[[ -z $(echo $PATH|grep $HOME/.local/bin) ]] &&
-{
+# add path program automatically
+if [[ -z $(echo $PATH|grep $HOME/.local/bin) ]]; then
+
     # shell file config
     shell_file=$HOME/.$(egrep -o "[^/]*$" <<< $SHELL)rc
 
@@ -21,25 +25,26 @@ chmod +x get-tor-bridges
     [[ ! -e "$shell_file" ]] && echo -e "\e[31mError cant find shell config!\e[m"
 
     # automatically add path program to shell config file jsut support bash and zshrc
-    [[ ! -z $(egrep "bash|zsh" <<< $shell_file) ]] &&
-    {
+    if [[ ! -z $(egrep "bash|zsh" <<< $shell_file) ]]; then
         # add path run script into PATH variable
-        [[ -z $(cat $shell_file|egrep "if \[ -e ~/.local/bin \]; then") ]] &&
-        {
+        if [[ -z $(cat $shell_file|egrep "if \[ -e ~/.local/bin \]; then") ]]; then
+
             echo "if [ -e ~/.local/bin ]; then" >> $shell_file
             echo "    export PATH=\"\$PATH:\$HOME/.local/bin/\"">> $shell_file
             echo "fi" >> $shell_file
             echo -e "\e[1;32mrun path was added automatically.\e[m"
-        } || echo -e "\e[1;32mpath already added.\e[m"
-    }||
-    {
+        else
+            echo -e "\e[1;32mpath already added.\e[m"
+        fi
+    else
         # manual installation
         echo -e "\e[1;33mPlease add these lines to your '${SHELL}' config file and continue with manual installation\e[1;35m"
         echo -e "if [ -e ~/.local/bin ]; then"
         echo -e "    export PATH=\"\$PATH:\$HOME/.local/bin/\""
         echo -e "fi"
-
-    }
-}
+    fi
+else
+    echo -e "\e[1;32m$HOME/.local/bin already added in to PATH variable.\e[m"
+fi
 
 echo -e "\e[1;32mScript installed successfully!\e[m"
