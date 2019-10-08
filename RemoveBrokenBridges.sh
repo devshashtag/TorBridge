c4b462ad56e6a97bed2a90bd8592a7592cd99662
MicroRobotProgramer <programmert3ster@gmail.com>
2019-10-08 17:23:07 +0330

    fixed bugs.args allow added . help added

--- RemoveBrokenBridges.sh
+++ RemoveBrokenBridges.sh
@@ -2,9 +2,29 @@
 
 # read status tor service and remove broken bridges
 
-tor_conf_file=/etc/tor/torrc
+TorConfigFile="/etc/tor/torrc"
 
+
+# this script need to run as root
+if [[ $UID -ne 0 ]]; then
+    echo -e "\e[31mThis script must be run as root !\e[m "
+    exit 1
+fi
+
+# usage
+function usage(){
+    echo -e "\e[0;1;33mPart of Tor Bridges -> Bridge Manager"
+    echo -e "\e[35mUSAGE :"
+    echo -e "\e[0;32m\t-c | --disable-check-bridges\t\e[0;36mdont try to find broken bridges"
+    echo -e "\e[0;32m\t-r | --reset-tor\t\e[0;36mrestart tor service"
+    echo -e "\e[0;32m\t-h | --help\t\t\e[0;36mshow this help"
+    echo -e "\e[m"
+}
+
+# read unable connetion bridges from status tor and comment bad bridges
 function remove_broken_bridges(){
+
+    echo -ne "\e[1;33mwaiting for find broken bridges:\n\t"
     bad_bridges=$(systemctl status tor.service|grep "unable"|egrep -o "([0-9]{1,3}.){3}[0-9]{1,3}:[0-9]{2,8}")
     [[ ! -z $(echo $bad_bridges|tr -d '\n') ]] &&
     {
@@ -19,17 +39,33 @@ function remove_broken_bridges(){
         echo -e "\e[33mbroken bridges successfully removed.\e[m"
     } ||
         echo -e "\e[1;35mAll bridges are healthy\e[m"
-    # restart tor
-    systemctl restart tor.service
-    echo -e "\e[1;35mwaiting for restart tor service .."
-    sleep 5
-
 }
+check="True"
+# args
+while [ "$1" != "" ]; do
+    case $1 in
+        -c | --disable-check-bridges ) check="False" ;;
 
-# this script need to run as root
-if [[ $UID -ne 0 ]]; then
-    echo -e "\e[31mThis script must be run as root !\e[m "
-    exit 1
+        -r | --reset-tor    )
+        {
+            # restart tor
+            systemctl restart tor.service
+            echo -e "\e[1;35mwaiting for restart tor service .."
+
+            status=" "
+            while [[ "$res" != "100" ]];do
+                sleep 0.1
+                res=$(systemctl status tor.service |egrep -o "Bootstrapped[^%]*"|tail -n 1|cut -d' ' -f2)
+                [[ -z $(grep "$res" <<< "$status") ]] && status="$res$status" && echo -ne "|$res"
+            done
+            echo "|"
+        };;
+
+        -h | --help | *) usage && exit 0;;
+    esac
+    shift
+done
+
+if [[ $check == "True" ]];then
+    remove_broken_bridges
 fi
-
-remove_broken_bridges
--- TorBridges.sh
+++ TorBridges.sh
@@ -4,8 +4,10 @@
 HTML_FILE="img.html"
 IMAGE_FILE="captcha.jpg"
 BridgeFile="bridges.txt"
-
+TorConfigFile="/etc/tor/torrc"
+IpSite="icanhazip.com"
 LinkGetTorBridge="https://bridges.torproject.org/bridges?transport=obfs4"
+
 
 # requirements for run this script
 if [[ ! $(which feh) || ! $(which tor) || ! $(which proxychains4) || ! $(which obfs4proxy)  ]]; then
@@ -13,27 +15,48 @@ if [[ ! $(which feh) || ! $(which tor) || ! $(which proxychains4) || ! $(which o
     exit 1
 fi
 
+# This script need to run as root
 if [[ $UID -ne 0 ]]; then
     echo -e "\e[31mThis script must be run as root!\e[m "
     exit 1
 fi
 
-# delete tmp files
+# usage
+function usage(){
+    echo -e "\e[0;1;33mA Simple Script for get tor bridge from\e[1;35m https://bridges.torproject.org/bridges\e[m"
+    echo -e "\e[35mUSAGE :"
+    echo -e "\e[0;32m\t-a | --add-bridges\t\e[0;36madd bridges to /etc/tor/torrc"
+    echo -e "\e[0;32m\t-t | --test-bridges\t\e[0;36mtest bridges and comment broken bridges"
+    echo -e "\e[0;32m\t-r | --reset-tor\t\e[0;36mrestart tor service"
+    echo -e "\e[0;32m\t-h | --help\t\t\e[0;36mshow this help"
+    echo -e "\e[m"
+}
+
+# delete files
 function ClearFiles() {
     for file in $@; do
-        if [ -e $file ]; then
+        if [[ -e "$file" ]]; then
             rm $file
         fi
     done
 }
-# delete tmp files
-ClearFiles "${HTML_FILE}" "${IMAGE_FILE}" "${BridgeFile}"
-
-# reset terminal
-tput reset
 
 # get tor bridges
 function get_tor_bridges(){
+
+    # add bridges into file $TorConfigFile (default=True)
+    AddBridges="$1"
+
+    # remove broken bridges (default=True)
+    RemoveBrokenBridges="$2"
+
+    # restart Tor Service (default=True)
+    ReTor="$3"
+
+
+    # delete tmp files
+    ClearFiles "${HTML_FILE}" "${IMAGE_FILE}" "${BridgeFile}"
+
     # get Captcha
     proxychains4 -q curl -s "$LinkGetTorBridge" -o $HTML_FILE
 
@@ -55,29 +78,29 @@ function get_tor_bridges(){
     # Enter code captcha
     while [[ -z $Cap_Response ]]; do
         # show ip
-        echo -ne "\e[35m[ "
-        proxychains4 -q curl ipecho.net/plain
+        echo -ne "\e[1;35m[ $(proxychains4 -q curl -s "$IpSite") ] \e[1;34m"
+
 
         # get code from user
-        echo -ne " ]\e[1;34m Enter code (Enter 'r' For Reset Captcha): "
-        read Cap_Response
+        read -p "Enter code (Enter 'r' For Reset Captcha): " Cap_Response
 
         # reset captcha
-        [[ $Cap_Response == "r" ]] && {
-                                        # kill feh job
-                                        kill $FEH_PID
+        [[ $Cap_Response == "r" ]] &&
+        {
+            # kill feh job
+            kill $FEH_PID
 
-                                        # delete tmp files
-                                        ClearFiles "${HTML_FILE}" "${IMAGE_FILE}" "${BridgeFile}"
+            # delete tmp files
+            ClearFiles "${HTML_FILE}" "${IMAGE_FILE}" "${BridgeFile}"
 
-                                        # emtpy capcha for while
-                                        Cap_Response=""
+            # clear data in Cap_Response for while condition true
+            Cap_Response=""
 
-                                        # reset captcha
-                                        get_tor_bridges
+            # reset captcha
+            get_tor_bridges "$AddBridges" "$RemoveBrokenBridges" "$ReTor"
 
-                                        exit 0
-                                    }
+            exit 0
+        }
 
         # slove captcha and get Bridges
         proxychains4 -q curl -s "https://bridges.torproject.org/bridges?transport=obfs4" \
@@ -86,15 +109,14 @@ function get_tor_bridges(){
         # cut bridges from html file(if code is incorrect bridges file is empty)
         RES=$(cat "$BridgeFile" |grep obfs4 |egrep -o "^[^<]*")
 
-        # if Cap_Response is correct. bridges save into /etc/tor/torrc . incorrect show error
+        # if Cap_Response is correct. bridges save into $TorConfigFile . incorrect show error
         if [[ ! -z $(echo $RES|tr -d '\n') ]]; then
             BRIDGES=$(echo "$RES" |sed 's/^/Bridge /g')
         else
             echo -e "\e[1;33mThe code entered is incorrect! try again ..\e[m"
+            # continue . True while
             Cap_Response=""
-            continue
         fi
-
     done
 
     # kill feh job
@@ -104,32 +126,61 @@ function get_tor_bridges(){
     ClearFiles "${HTML_FILE}" "${IMAGE_FILE}" "${BridgeFile}"
 
     # add Bridges Into torrc
-    if [ -e "/etc/tor/torrc" ]; then
+    if [ -e "$TorConfigFile" ]; then
 
-        # add 'UseBridges 1' in to /etc/tor/torrc if not exist
-        if [[ ! $(grep "UseBridges 1" /etc/tor/torrc) ]]; then
-            echo "UseBridges 1" >> /etc/tor/torrc
+        # add 'UseBridges 1' in to $TorConfigFile if not exist
+        if [[ ! $(grep "UseBridges 1" $TorConfigFile) ]]; then
+            echo "UseBridges 1" >> $TorConfigFile
         fi
 
-        # add 'ClientTransportPlugin obfs4 exec /usr/bin/obfs4proxy' in to /etc/tor/torrc if not exist
-        if [[ ! $(grep "ClientTransportPlugin obfs4 exec" /etc/tor/torrc) ]]; then
+        # add 'ClientTransportPlugin obfs4 exec /usr/bin/obfs4proxy' into $TorConfigFile if not exist
+        if [[ ! $(grep "ClientTransportPlugin obfs4 exec" $TorConfigFile) ]]; then
             obfs4proxy=$(which obfs4proxy)
-            echo "ClientTransportPlugin obfs4 exec ${obfs4proxy}" >> /etc/tor/torrc
+            echo "ClientTransportPlugin obfs4 exec ${obfs4proxy}" >> $TorConfigFile
         fi
 
-        sudo echo -e "\n${BRIDGES}" >> /etc/tor/torrc
-        echo -e "\e[35mBridges Added into \e[35m/etc/tor/torrc\n"
+        if [[ "$AddBridges" == "True" ]]; then
+            sudo echo -e "\n${BRIDGES}" >> $TorConfigFile
+            echo -e "\e[35mBridges Added into \e[35m$TorConfigFile\n"
+        fi
 
+        # show bridges
         echo -e "\e[36m${BRIDGES}"
 
-        echo -e "\e[1;33mwaiting for remove broken bridges .."
-        remove-broken-bridges
+        if [[ "$RemoveBrokenBridges" == "True" ]]; then
+            remove-broken-bridges
+        fi
+
+        if [[ "$ReTor" == "True" ]]; then
+            remove-broken-bridges -c -r
+        fi
+
     else
-        echo -e "\e[31m /etc/tor/torrc doesn't exist"
+        echo -e "\e[31m $TorConfigFile doesn't exist"
         exit 0
     fi
 }
 
+# # reset terminal
+# tput reset
 
 
-get_tor_bridges
+AddBridges="False"
+RemoveBrokenBridges="False"
+ReTor="False"
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -a | --add-bridges  ) AddBridges="True" ;;
+
+        -t | --test-bridges ) RemoveBrokenBridges="True" ;;
+
+        -r | --reset-tor    ) ReTor="True";;
+
+        -h | --help | *) usage && exit 0;;
+    esac
+    shift
+done
+
+# get tor bridges
+get_tor_bridges "$AddBridges" "$RemoveBrokenBridges" "$ReTor"
